4af8d960265ae044129ffc806cc736bc2a339383
Manuel Pretelin <m_pretelin@hotmail.com>
2019-09-30 19:37:08 -0500

    chore(loading): add logic to check device status

--- plugin/src/screens/LoadingScreen.js
+++ plugin/src/screens/LoadingScreen.js
@@ -1,11 +1,45 @@
 import * as React from "react";
+import axios from "axios";
 import { ActivityIndicator, Dimensions, View } from "react-native";
+import { getAdobeAuthorizationHeader } from '../utils/index';
+import { getAppData } from "@applicaster/zapp-react-native-bridge/QuickBrick";
 
-const { width, height } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
 
 class LoadingScreen extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    const {
+      environment_url,
+      requestor_id,
+      public_key,
+      secret
+    } = this.props.screenData.general
+
+    axios.get(`https://${environment_url}/api/v1/tokens/authn?deviceId=${getAppData().uuid}&requestor=${requestor_id}`,
+      {
+        headers: {
+          "Authorization": getAdobeAuthorizationHeader(
+            'POST',
+            requestor_id,
+            '/authn',
+            public_key,
+            secret
+          )
+        }
+      })
+      .then(async res => {
+        if (res.status === 200) {
+          this.props.goToScreen('WELCOME')
+
+        } else {
+          this.props.goToScreen('SIGNIN')
+        }
+      })
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -19,9 +53,9 @@ class LoadingScreen extends React.Component {
 
 const styles = {
   container: {
-    width,
+    flex: 1,
     height,
-    backgroundColor: "#E2E2E2",
+    backgroundColor: "transparent",
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
